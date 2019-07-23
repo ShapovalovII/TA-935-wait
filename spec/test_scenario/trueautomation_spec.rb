115df5b7b2cd9c4dd76e54a96af34a6c12e3a0da
Igor Shapovalov <shapovalovii2000@gmail.com>
2019-07-23 17:02:28 +0300

    first commit

---
+++ spec/test_scenario/trueautomation_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'TrueAutomation.IO capybara example' do
+  scenario 'Test example' do
+    visit 'https://trueautomation.io/'
+
+    find(ta("testName")).click
+  end
+end
